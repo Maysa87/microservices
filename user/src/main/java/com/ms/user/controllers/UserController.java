472d80ad1a093ef220e89279e2460976740c3375
Maysa Leocadio <lmaysagomes@gmail.com>
2025-02-05 13:31:23 -0300

    feat: converting DTOs to entities using BeanUtils

--- user/src/main/java/com/ms/user/controllers/UserController.java
+++ user/src/main/java/com/ms/user/controllers/UserController.java
@@ -1,7 +1,22 @@
 package com.ms.user.controllers;
 
+import com.ms.user.dtos.UserRecordDto;
+import com.ms.user.entity.UserEntity;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    @PostMapping("/users")
+    public ResponseEntity<UserEntity> saveUser(@RequestBody @Valid UserRecordDto userRecordDto){
+        var userEntity = new UserEntity();
+        BeanUtils.copyProperties(userRecordDto, userEntity);
+        return ResponseEntity.status(HttpStatus.CREATED).body();
+    }
 }

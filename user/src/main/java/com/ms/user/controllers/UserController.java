8eeb645ce3499cb5fdc3d27c31eb76e170aa0a26
Maysa Leocadio <lmaysagomes@gmail.com>
2025-02-11 11:08:24 -0300

    feat: injection point UserService into UserController

--- user/src/main/java/com/ms/user/controllers/UserController.java
+++ user/src/main/java/com/ms/user/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.ms.user.controllers;
 
 import com.ms.user.dtos.UserRecordDto;
 import com.ms.user.entity.UserEntity;
+import com.ms.user.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
@@ -13,10 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
     @PostMapping("/users")
     public ResponseEntity<UserEntity> saveUser(@RequestBody @Valid UserRecordDto userRecordDto){
         var userEntity = new UserEntity();
         BeanUtils.copyProperties(userRecordDto, userEntity);
-        return ResponseEntity.status(HttpStatus.CREATED).body();
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userEntity));
     }
 }
--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -1,7 +1,9 @@
 package com.ms.user.services;
 
+import com.ms.user.entity.UserEntity;
 import com.ms.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -9,5 +11,9 @@ public class UserService {
 
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
+    }
+    @Transactional
+    public UserEntity save(UserEntity userEntity){
+        return userRepository.save(userEntity);
     }
 }

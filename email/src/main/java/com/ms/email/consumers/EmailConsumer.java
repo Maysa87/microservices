c4ced5ff87d514173bba426105d15f411d1c9862
Maysa Leocadio <lmaysagomes@gmail.com>
2025-06-26 16:53:03 -0300

    feat: updating

--- email/src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,15 +1,24 @@
 package com.ms.email.consumers;
 
-import com.ms.email.dtos.EmailDto;
+import com.ms.email.dtos.EmailRecordDto;
+import com.ms.email.entity.EmailEntity;
+import com.ms.email.service.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
+    final EmailService emailService;
 
+    public EmailConsumer(EmailService emailService){
+        this.emailService = emailService;
+    }
     @RabbitListener(queues = "${broker.queue.email.name}" )
-    public void listenEmailQueue(@Payload EmailDto emailDto){
-        System.out.println(emailDto.getEmailTo());
+    public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto){
+        var emailEntity = new EmailEntity();
+        BeanUtils.copyProperties(emailRecordDto, emailEntity);
+        emailService.sendEmail(emailEntity);
     }
 }
--- email/src/main/java/com/ms/email/entity/EmailEntity.java
+++ email/src/main/java/com/ms/email/entity/EmailEntity.java
@@ -29,4 +29,63 @@ public class EmailEntity implements Serializable {
     public UUID getEmailId() {
         return emailId;
     }
+    public void setEmailId(UUID emailId) {
+        this.emailId = emailId;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getEmailFrom() {
+        return emailFrom;
+    }
+
+    public void setEmailFrom(String emailFrom) {
+        this.emailFrom = emailFrom;
+    }
+
+    public String getEmailTo() {
+        return emailTo;
+    }
+
+    public void setEmailTo(String emailTo) {
+        this.emailTo = emailTo;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getSendDateEmail() {
+        return sendDateEmail;
+    }
+
+    public void setSendDateEmail(LocalDateTime sendDateEmail) {
+        this.sendDateEmail = sendDateEmail;
+    }
+
+    public StatusEmail getStatusEmail() {
+        return statusEmail;
+    }
+
+    public void setStatusEmail(StatusEmail statusEmail) {
+        this.statusEmail = statusEmail;
+    }
 }
